7708295e32a63597de86174a0bbfc6dfa21d7edb
Mehakpreet Kaur <114038404+Mehakpreet123@users.noreply.github.com>
2025-03-14 16:22:12 +0530

    project level

--- configuration files(gradle)/build.gradle.kts
+++ configuration files(gradle)/build.gradle.kts
@@ -1,67 +1,7 @@
+
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.google.services) apply false
 }
 
-android {
-    namespace = "com.example.siteprogress"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.example.siteprogress"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        viewBinding = true
-    }
-}
-
-dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.annotation)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.firebase.appcheck.playintegrity)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
-    // Firebase BOM (Ensures version consistency)
-    implementation(platform(libs.firebase.bom))
-
-    // Firebase dependencies (without specifying versions)
-    implementation("com.google.firebase:firebase-firestore-ktx")
-    implementation("com.google.firebase:firebase-auth-ktx")
-    implementation("com.google.firebase:firebase-storage-ktx")
-
-    implementation ("com.google.firebase:firebase-auth:22.1.2")
-    implementation ("com.google.firebase:firebase-appcheck-playintegrity:16.0.1")
-}
-
-plugins.apply("com.google.gms.google-services") // Add this line!
